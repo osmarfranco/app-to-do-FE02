30f996f3c8a3e66800b632c791a40d18e4b67257
bczeulli <bczeulli@gmail.com>
2022-06-29 20:46:31 -0300

    get tasks por user

--- scripts/tasks/tasks.js
+++ scripts/tasks/tasks.js
@@ -9,6 +9,7 @@ if (!tokenJwt) {
     alert("You shall not pass!!")
 } else {
     getDataUser(tokenJwt)
+    getUserTasks(tokenJwt)
 }
 
 }
@@ -42,8 +43,61 @@ async  function getDataUser(token) {
 
 }
 
+async  function getUserTasks(token) {
+
+    
+
+    let configRequest = {
+        headers: {
+            "Authorization": token
+        }
+    }
+    try {
+        let data = await fetch(ENDPOINT_TASKS, configRequest)
+
+        if (data.status == 200) {
+            let responseConvert = await data.json();
+            
+
+            getTasks(responseConvert)
+
+        } else {
+            throw "Problema ao buscar tarefas"
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+    
+
+}
+
 function nameInNavBar(objUser) {
     let p = document.querySelector("#nomeUsuario")
 
     p.innerText = `${objUser.firstName} ${objUser.lastName}`
+}
+
+
+
+
+function getTasks(tasks){
+
+    const SKELETON = document.getElementById('skeleton')
+    console.log(tasks)
+
+    for ( i of tasks){
+        
+        let itemlist = `<li class="tarefa">
+        <div class="not-done"></div>
+        <div class="descricao">
+          <p class="nome">${i.description}</p>
+          <p class="timestamp">${i.createdAt}1</p>
+        </div>
+      </li>`
+      SKELETON.innerHTML += itemlist
+        
+
+    }
+
 }
