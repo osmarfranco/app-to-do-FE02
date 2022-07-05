9bc2dedf0f2d5161530f32daa3870ea5535fbe44
Renato L <renato.linoeu@hotmail.com>
2022-07-04 23:24:40 -0300

    modal, btn delete e return
    Adicionado o botão delete e return,  adiciona dados ao modal dinamicamente

--- scripts/tasks/move_task.js
+++ scripts/tasks/move_task.js
@@ -1,5 +1,6 @@
 let listaUL = document.querySelector(".tarefas-pendentes")
 let listaULTerminadas = document.querySelector(".tarefas-terminadas")
+let divModal = document.getElementById("message-text")
 
 //Altera o completed para true
 async  function editTasks(id, token, JSON){
@@ -15,14 +16,7 @@ async  function editTasks(id, token, JSON){
         body: JSON
     }
     try {
-        let data = await fetch(ENDPOINT_TASKS + '/' + id, configRequest)
-        if (data.status == 200 || data.status == 201) {
-            let responseConvert = await data.json();
-            console.log(responseConvert);
-
-        } else {
-            throw "Problema ao buscar tarefas"
-        }
+        await fetch(ENDPOINT_TASKS + '/' + id, configRequest)
 
     } catch (error) {
         console.log(error)
@@ -33,7 +27,6 @@ async  function editTasks(id, token, JSON){
 
 //Altera o completed para false
 async  function returnTasks(id, token, JSON){
-    
     let configRequest = {
         method: "PUT",
         headers: {
@@ -45,14 +38,54 @@ async  function returnTasks(id, token, JSON){
         body: JSON
     }
     try {
-        let data = await fetch(ENDPOINT_TASKS + '/' + id, configRequest)
-        if (data.status == 200 || data.status == 201) {
+        await fetch(ENDPOINT_TASKS + '/' + id, configRequest)
+
+    } catch (error) {
+        console.log(error)
+        toastError()
+    }
+}
+
+//Deletar tarefa 
+async  function dellTask(id, token){
+    let configRequest = {
+        method: "DELETE",
+        headers: {
+            "Authorization": token           
+        },
+        redirect: 'follow'
+}
+try {
+    await fetch(ENDPOINT_TASKS + "/" + id , configRequest)
+
+} catch (error) {
+    console.log(error)
+    toastError()
+}
+}
+
+//coleta dados das tasks para o modal dinamico
+async  function coletaDadosTasks(id, token){
+    let configRequest = {
+        method: "GET",
+        headers: {
+            "Authorization": token,
+            "id": id
+            
+        },
+        redirect: 'follow'
+    }
+    try {
+        let data = await fetch(ENDPOINT_TASKS + "/" + id , configRequest)
+        if (data.status == 200) {
             let responseConvert = await data.json();
-            console.log(responseConvert);
+            divModal.innerHTML =`${responseConvert.description}`
+            console.log(responseConvert.id);
 
         } else {
             throw "Problema ao buscar tarefas"
         }
+
 
     } catch (error) {
         console.log(error)
@@ -60,6 +93,8 @@ async  function returnTasks(id, token, JSON){
     }
 
 }
+
+
 
 //pega as informções e renderiza as tarefas na tela
 function renderizaTarefas(tarefa){
@@ -70,7 +105,7 @@ function renderizaTarefas(tarefa){
         li.innerHTML = 
         `
         <div class="not-done" id="${tarefas.id}" onClick="trocarTarefa(${tarefas.id})"> </div>
-        <div class="descricao" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <div class="descricao" onClick="passarDadosModal(${tarefas.id})" data-bs-toggle="modal" data-bs-target="#exampleModal">
             <p class="nome">${tarefas.description}</p>
             <p class="timestamp"><i class="far fa-calendar-alt"></i> ${tarefas.createdAt}</p>
         </div>
@@ -88,7 +123,7 @@ function renderizaTarefas(tarefa){
             <p class="nome">${tarefas.description}</p>
             <div>
                 <button><i id="${tarefas.id}" class="fas fa-undo-alt change" onClick="voltarTarefa(${tarefas.id})"></i></button>
-                <button><i id="${tarefas.id}" class="far fa-trash-alt"></i></button>
+                <button onClick="deletaTarefa(${tarefas.id})"><i id="${tarefas.id}" class="far fa-trash-alt" ></i></button>
             </div>
         </div>
 
@@ -103,10 +138,29 @@ function renderizaTarefas(tarefa){
 function trocarTarefa(id){
     let JSON = "{\r\n  \"completed\": true\r\n}";
     editTasks(id, sessionStorage.getItem("jwt"), JSON)
+  
 }
 
 // função que é chamada quando se clica no botão da class "fas fa-undo-alt change"
 function voltarTarefa(id){
     let JSON = "{\r\n  \"completed\": false\r\n}";
     returnTasks(id, sessionStorage.getItem("jwt"), JSON)
+}
+
+// 
+function deletaTarefa(id){  
+    dellTask(id, sessionStorage.getItem("jwt"))
+}
+
+
+
+// Modal vamos ver se rola
+
+// pede os dados para a criação do modal dinamico
+function passarDadosModal(id){
+    coletaDadosTasks(id, sessionStorage.getItem("jwt"))
+}
+
+function limpaDados(){
+    divModal.innerHTML = ``
 }
